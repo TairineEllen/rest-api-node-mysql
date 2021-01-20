2ef8ed9a7633e920f17592373f38eb49a6207ee1
TairineEllen <tairine.ellen@gmail.com>
2021-01-20 18:41:13 -0300

    refactor: rename createAppointments to addAppointment

--- src/controllers/appointmentsController.js
+++ src/controllers/appointmentsController.js
@@ -2,11 +2,11 @@ const getAppointments = (req, res) => {
   res.send('Rota de atendimentos GET');
 };
 
-const createAppointments = (req, res) => {
+const addAppointment = (req, res) => {
   res.send('Rota de atendimentos POST');
 };
 
 module.exports = {
   getAppointments,
-  createAppointments
+  addAppointment
 };
--- src/routes/appointmentsRoute.js
+++ src/routes/appointmentsRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/appointmentsController');
 
 router.get('/', controller.getAppointments);
-router.post('/', controller.createAppointments);
+router.post('/', controller.addAppointment);
 
 module.exports = router;
