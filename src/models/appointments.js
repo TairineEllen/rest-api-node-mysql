1cd6e3105ac5e1b25d0bd20f8a46ca0d326b85af
TairineEllen <tairine.ellen@gmail.com>
2021-01-20 21:47:36 -0300

    feat: add date to the appointment table

--- src/models/appointments.js
+++ src/models/appointments.js
@@ -1,10 +1,14 @@
 const connection = require('../infra/connection');
+const moment = require('moment');
 
 class Appointment {
   add(appointment) {
-    const sql = 'INSERT INTO Atendimentos SET ?';
+    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+    const data = moment(appointment.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+    const appointmentWithDate = { ...appointment, dataCriacao, data };
+    const sql = 'INSERT INTO Atendimentos SET ?';    
 
-    connection.query(sql, appointment, (error, results) => {
+    connection.query(sql, appointmentWithDate, (error, results) => {
       if (error) {
         console.log(error);
       } else {
